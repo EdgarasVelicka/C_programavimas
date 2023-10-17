269689f5a4ba5b1d71734825f618c1c25fdd00a9
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-17 23:06:39 +0300

    Add project files.

---
+++ _01_Kintamieji/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pagrindinė informacija
+            string vardas = "Edgaras";
+            string pavarde = "Velička";
+            int amzius = 36;
+
+            // Papildoma informacija
+            float ugis = 1.80f; // metrai
+            var svoris = 96; // kilogramai
+
+            // Informacija ape aukštąją mokyklą
+            string aukstojiMokykla = "Mykolo Romerio universitetas";
+            string akademinesGrupesKodas = "TV0752545";
+            string kursas = "IV";
+            string studijuProgramosPavadinimas = "Teisė ir valdymas";
+            var atsiskaitytuKredituSkaicius = 120;
+        }
+    }
+}
