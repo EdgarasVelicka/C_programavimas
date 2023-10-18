05cd9b7c1fc6994b6cf26e4c5a6db4e10691d768
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-18 09:14:56 +0300

    3 Uzduotis

---
+++ kvadratas_3 uzduotis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvadratas_3_uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
